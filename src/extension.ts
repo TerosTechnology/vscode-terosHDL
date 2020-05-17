15ce349436099663ecaf5b385fe860f67c72957c
Carlos Alberto <carlos.ruiz@softwareradiosystems.com>
2020-05-17 14:33:00 +0200

    header

--- src/extension.ts
+++ src/extension.ts
@@ -5,16 +5,19 @@ import * as templates from "./lib/templates/templates";
 import * as formatter from "./lib/formatter/formatter";
 import * as documentation from "./lib/documenter/documenter";
 import * as linter from "./lib/linter/linter";
-import {provideDocumentFormattingEdits} from "./lib/formatter/formatter";
+// import {provideDocumentFormattingEdits} from "./lib/formatter/formatter";
 
 let linter_vhdl;
 let linter_verilog;
+let current_context;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "teroshdl" is now active!');
+	//Context
+	current_context = context;
 	//Templates
 	context.subscriptions.push(vscode.commands.registerCommand('teroshdl.generate_template', templates.get_template));
 	//Formatter
@@ -40,3 +43,35 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+export async function provideDocumentFormattingEdits(
+    document: vscode.TextDocument,
+    options: vscode.FormattingOptions,
+    token: vscode.CancellationToken
+  ): Promise<vscode.TextEdit[]> {
+    const edits: vscode.TextEdit[] = [];
+
+    let code : string = document.getText();
+    let opt = options;
+    let code_format : string;
+    if (document.languageId == "vhdl"){
+        code_format = formatter.format_vhdl(code);
+    }
+    else {
+        code_format = formatter.format_verilog(code,current_context);
+    }
+    //Error
+    if (code_format == null){
+        // vscode.window.showErrorMessage('Select a valid file.!');
+        console.log("Error format code.");
+        return edits;
+    }
+    else{
+        const replacement = vscode.TextEdit.replace(
+            formatter.getDocumentRange(document),
+            code_format
+        );
+        edits.push(replacement);
+        return edits;
+    }
+}
--- src/lib/formatter/formatter.ts
+++ src/lib/formatter/formatter.ts
@@ -20,16 +20,13 @@
 // along with Colibri.  If not, see <https://www.gnu.org/licenses/>.
 import * as vscode from 'vscode';
 import * as jsteros from 'jsteros';
-
-import * as child from 'child_process';
-import * as temp from 'temp';
-import * as fs from 'fs';
+import * as formatter_verilog from "./formatter_verilog";
 
 export async function format(){
     vscode.commands.executeCommand("editor.action.formatDocument")
 }
 
-function format_vhdl(code: string) {
+export function format_vhdl(code: string) {
     let configuration : vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('teroshdl');
     let settings = {
       "RemoveComments": configuration.get('formatting.vhdl.remove-comments'),
@@ -66,39 +63,7 @@ function format_vhdl(code: string) {
     return code_format;
   }
 
-  export async function provideDocumentFormattingEdits(
-    document: vscode.TextDocument,
-    options: vscode.FormattingOptions,
-    token: vscode.CancellationToken
-  ): Promise<vscode.TextEdit[]> {
-    const edits: vscode.TextEdit[] = [];
-
-    let code : string = document.getText();
-    let opt = options;
-    let code_format : string;
-    if (document.languageId == "vhdl"){
-        code_format = format_vhdl(code);
-    }
-    else {
-        code_format = format_verilog(code);
-    }
-    //Error
-    if (code_format == null){
-        // vscode.window.showErrorMessage('Select a valid file.!');
-        console.log("Error format code.");
-        return edits;
-    }
-    else{
-        const replacement = vscode.TextEdit.replace(
-            getDocumentRange(document),
-            code_format
-        );
-        edits.push(replacement);
-        return edits;
-    }
-}
-
-const getDocumentRange = (document: vscode.TextDocument): vscode.Range => {
+export const getDocumentRange = (document: vscode.TextDocument): vscode.Range => {
     const lastLineId = document.lineCount - 1;
     return new vscode.Range(
       0,
@@ -108,50 +73,6 @@ const getDocumentRange = (document: vscode.TextDocument): vscode.Range => {
     );
 };
 
-const style_map: { [style: string]: string } = {
-    "Indent only": "",
-	"Kernighan&Ritchie": "kr",
-	"GNU": "gnu",
-	"ANSI": "ansi"
-};
-
-function format_verilog(code: string) {
-	const iStylePath = "/home/carlos/repo/vscode-terosHDL/src/lib/formatter/bin/iStyle";
-	const iStyleExtraArgs = "";
-	var args: string[] = [
-		"-n", // Do not create a .orig file
-	];
-	const style = get_formatting_style_arg();
-	if (style.length !== 0) {
-		args.push(style);
-	}
-	if (iStyleExtraArgs.length !== 0) {
-		args = args.concat(iStyleExtraArgs.split(" "));
-	}
-	var tempfile: string = create_temp_file_of_code(code);
-	args.push(tempfile);
-    child.execFileSync(iStylePath, args, {});
-    let formatted_code = fs.readFileSync(tempfile, { encoding: "utf8" });
-    return formatted_code;
+export function format_verilog(code: string, context: vscode.ExtensionContext) {
+    return formatter_verilog.format_verilog(code,context);
 }
-
-function get_formatting_style_arg(): string {    
-    const style = <string>vscode.workspace.getConfiguration("teroshdl.formatting.verilog").get("style");
-    const map_style = style_map[style];
-	if (map_style !== undefined && map_style.length !== 0) {
-        return `--style=ansi`;
-	} else {
-		return "";
-	}
-}
-
-function create_temp_file_of_code(content: string) {
-	const temp_file = temp.openSync();
-	if (temp_file === undefined) {
-		throw "Unable to create temporary file";
-	}
-	fs.writeSync(temp_file.fd, content);
-	fs.closeSync(temp_file.fd);
-	return temp_file.path;
-}
-
