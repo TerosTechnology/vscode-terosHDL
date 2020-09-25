e436ab3afd573783487631b3489e2423b7842d64
Ismael_Pérez_R <34767708+smgl9@users.noreply.github.com>
2020-09-26 00:45:59 +0200

    eval signed

--- src/extension.ts
+++ src/extension.ts
@@ -177,12 +177,7 @@ export function activate(context: vscode.ExtensionContext) {
                         return;
                     }
                     let x = parseInt(number[1], 16);
-
-                    let pow_hex =  Math.pow(16,number[1].length);
-                    let x1 = parseInt(number[1][0], 16);
-                    if (x > pow_hex >> 1) {
-                        x1 = x-pow_hex;
-                    }
+                    let x1 = eval_signed_hex(number[1],x);
                     return new vscode.Hover(leadingText + ' = ' + x + ' (unsigned)  = ' + x1 + ' (signed)');
                 }
                 else if (/[0-1_]+"/g.test(leadingText)) {
@@ -192,7 +187,8 @@ export function activate(context: vscode.ExtensionContext) {
                         return;
                     }
                     let x = parseInt(number[0], 2);
-                    return new vscode.Hover('"' + leadingText + ' = ' + x + ' (unsigned)');
+                    let x1 =eval_signed_bin(number[0],x);
+                    return new vscode.Hover('"' + leadingText + ' = ' + x + ' (unsigned) = ' + x1 + ' (signed)');
                 }
             }
         }
@@ -210,7 +206,8 @@ export function activate(context: vscode.ExtensionContext) {
                         return;
                     }
                     let x = parseInt(number[1], 16);
-                    return new vscode.Hover(leadingText + ' = ' + x + ' (unsigned)');
+                    let x1 = eval_signed_hex(number[1],x);
+                    return new vscode.Hover(leadingText + ' = ' + x + ' (unsigned) = ' + x1 + ' (signed)');
                 }
                 else if (/b[0-1_]+/g.test(leadingText)) {
                     const regex = /b([0-1_]+)/g;
@@ -219,7 +216,8 @@ export function activate(context: vscode.ExtensionContext) {
                         return;
                     }
                     let x = parseInt(number[1], 2);
-                    return new vscode.Hover(leadingText + ' = ' + x + ' (unsigned)');
+                    let x1 =eval_signed_bin(number[1],x);
+                    return new vscode.Hover(leadingText + ' = ' + x + ' (unsigned) = ' + x1 + ' (signed)');
                 }
                 else if (/o[0-8_]+/g.test(leadingText)) {
                     const regex = /o([0-7_]+)/g;
@@ -228,7 +226,8 @@ export function activate(context: vscode.ExtensionContext) {
                         return;
                     }
                     let x = parseInt(number[1], 8);
-                    return new vscode.Hover(leadingText + ' = ' + x + ' (unsigned)');
+                    let x1 =eval_signed_oct(number[1],x);
+                    return new vscode.Hover(leadingText + ' = ' + x + ' (unsigned) = ' + x1 + ' (signed)');
                 }
             }
         }
@@ -331,4 +330,30 @@ function help_message(){
     });
 }
 
+function eval_signed_hex(number_s, int_number){
+    let pow_hex =  Math.pow(16,number_s.length);
+    let x1 = int_number;
+    if (int_number >= pow_hex >> 1) {
+        x1 = int_number-pow_hex;
+    }
+    return x1;
+}
+
+function eval_signed_bin(number_s, int_number){
+    let pow_bin =  1 << number_s.length-1;
+    let x1 = int_number;
+    if (int_number >= pow_bin >> 1) {
+        x1 = int_number-pow_bin;
+    }
+    return x1;
+}
+
+function eval_signed_oct(number_s, int_number){
+    let pow_oct =  Math.pow(8,number_s.length);
+    let x1 = int_number;
+    if (int_number >= pow_oct >> 1) {
+        x1 = int_number-pow_oct;
+    }
+    return x1;
+}
 
