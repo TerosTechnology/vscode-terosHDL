a301ceb8d385a6c1c816fa425b03438f8e1be453
Ismael_Pérez_R <34767708+smgl9@users.noreply.github.com>
2020-09-27 23:45:53 +0200

    add 2008 dialog

--- src/lib/templates/templates.ts
+++ src/lib/templates/templates.ts
@@ -32,9 +32,9 @@ export async function get_template() {
     let document = vscode.window.activeTextEditor.document;
     let language_id : string = document.languageId;
 
-    const TYPES_VHDL_INFO : string[] = ["cocotb", "VUnit testbench", "VHDL testbench", "Copy as component", "Copy as signal", "Copy as instance"];
-    const TYPES_VHDL      : string[] = ["cocotb", "tb", "tb", "component", "component", "component"];
-    const SUBTYPES_VHDL   : string[] = ["cocotb", "vunit", "normal", "component", "signals", "instance"];
+    const TYPES_VHDL_INFO : string[] = ["cocotb", "VUnit testbench", "VHDL testbench", "Copy as component", "Copy as signal", "Copy as instance","Copy as instance_2008"];
+    const TYPES_VHDL      : string[] = ["cocotb", "tb", "tb", "component", "component", "component", "component"];
+    const SUBTYPES_VHDL   : string[] = ["cocotb", "vunit", "normal", "component", "signals", "instance", "instance_vhdl_2008"];
 
     const TYPES_VERILOG_INFO : string[] = ["cocotb", "Verilator", "VUnit testbench", "Verilog testbench", "Copy as signal", "Copy as instance"];
     const TYPES_VERILOG      : string[] = ["cocotb", "verilator", "tb", "tb", "component", "component"];
