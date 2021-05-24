5092bcfd49a8c06856f21e56453498d6c75b36fe
Ismael_Pérez_R <34767708+smgl9@users.noreply.github.com>
2021-05-24 21:36:21 +0200

    clean code

--- src/lib/rusthdl/rust_hdl.ts
+++ src/lib/rusthdl/rust_hdl.ts
@@ -94,22 +94,6 @@ export async function run_rusthdl(ctx: ExtensionContext) {
         })
     );
 
-    output.appendLine('Checking for updates...');
-    // lockfile
-    //     .lock(ctx.asAbsolutePath('server'), {
-    //         lockfilePath: ctx.asAbsolutePath(path.join('server', '.lock')),
-    //     })
-    //     .then((release: () => void) => {
-    //         getLatestLanguageServer(60000, ctx)
-    //             .catch((err) => {
-    //                 output.appendLine(err);
-    //             })
-    //             .finally(() => {
-    //                 output.appendLine('Language server update finished.');
-    //                 return release();
-    //             });
-    //     });
-
     output.appendLine('Language server started');
 }
 
