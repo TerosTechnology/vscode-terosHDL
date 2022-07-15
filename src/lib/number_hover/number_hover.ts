63a735bba514c1bf2e8e8a9d18f1af8477b2d177
Carlos Alberto <carlos.ruiz@srs.io>
2022-07-15 14:43:34 +0200

    test

--- src/lib/number_hover/number_hover.ts
+++ src/lib/number_hover/number_hover.ts
@@ -1,113 +1,31 @@
 import * as vscode from 'vscode';
+import * as teroshdl2 from 'teroshdl2';
 
-export function vhdl_hover(document, position, token) {
+export function init_hover(context, vhdlSelector, verilogSelector){
+  let hover_numbers_vhdl = vscode.languages.registerHoverProvider(vhdlSelector, {
+    provideHover(document, position, token) {
+        return hover(document, position, teroshdl2.common_general.HDL_LANG.VHDL);
+    }
+  });
+  let hover_numbers_verilog = vscode.languages.registerHoverProvider(verilogSelector, {
+    provideHover(document, position, token) {
+        return hover(document, position, teroshdl2.common_general.HDL_LANG.VERILOG);
+    }
+  });
+  context.subscriptions.push(hover_numbers_vhdl);
+  context.subscriptions.push(hover_numbers_verilog);
+}
+
+function hover(document, position, lang: teroshdl2.common_general.HDL_LANG) {
   let wordRange = document.getWordRangeAtPosition(position, /\w[-\w\.\"]*/g);
+  let result : teroshdl2.hdl_hover.result_t;
   if (wordRange !== undefined) {
     let leadingText = document.getText(new vscode.Range(wordRange.start, wordRange.end));
-    if (/x"[0-9a-fA-F_]+"/g.test(leadingText)) {
-      const regex = /x"([0-9a-fA-F_]+)"/g;
-      let number = regex.exec(leadingText.replace('_', ''));
-      if (number === null || number[1] === null) {
-        return;
-      }
-      let x = parseInt(number[1], 16);
-      let x1 = eval_signed_hex(number[1], x);
-      if (x === x1) {
-        return new vscode.Hover(leadingText + ' = ' + x);
-      } else {
-        return new vscode.Hover(leadingText + ' = ' + x + ' (unsigned)  || ' + x1 + ' (signed)');
-      }
-    }
-    else if (/[0-1_]+"/g.test(leadingText)) {
-      const regex = /([0-1_]+)"/g;
-      let number = regex.exec(leadingText.replace('_', ''));
-      if (number === null || number[1] === null) {
-        return;
-      }
-      let x = parseInt(number[0], 2);
-      let x1 = eval_signed_bin(number[0], x);
-      if (x === x1) {
-        return new vscode.Hover('"' + leadingText + ' = ' + x);
-      } else {
-        return new vscode.Hover('"' + leadingText + ' = ' + x + ' (unsigned) || ' + x1 + ' (signed)');
-      }
+    result = teroshdl2.hdl_hover.hdl_hover(leadingText, lang);
+    if (result.is_multi === false) {
+      return new vscode.Hover(`${leadingText} = ${result.unsigned_n}`);
+    } else {
+      return new vscode.Hover(`${leadingText} = ${result.unsigned_n} (unsigned) || ${result.signed_n} (signed)`);
     }
   }
-}
-
-export function verilog_hover(document, position, token) {
-  let wordRange = document.getWordRangeAtPosition(position, /\w[-\w\.\']*/g);
-  if (wordRange !== undefined) {
-    let leadingText = document.getText(new vscode.Range(wordRange.start, wordRange.end));
-    if (/h[0-9a-fA-F_]+/g.test(leadingText)) {
-      const regex = /h([0-9a-fA-F_]+)/g;
-      let number = regex.exec(leadingText.replace('_', ''));
-      if (number === null || number[1] === null) {
-        return;
-      }
-      let x = parseInt(number[1], 16);
-      let x1 = eval_signed_hex(number[1], x);
-      if (x === x1) {
-        return new vscode.Hover(leadingText + ' = ' + x);
-      } else {
-        return new vscode.Hover(leadingText + ' = ' + x + ' (unsigned) || ' + x1 + ' (signed)');
-      }
-    }
-    else if (/b[0-1_]+/g.test(leadingText)) {
-      const regex = /b([0-1_]+)/g;
-      let number = regex.exec(leadingText.replace('_', ''));
-      if (number === null || number[1] === null) {
-        return;
-      }
-      let x = parseInt(number[1], 2);
-      let x1 = eval_signed_bin(number[1], x);
-      if (x === x1) {
-        return new vscode.Hover(leadingText + ' = ' + x);
-      } else {
-        return new vscode.Hover(leadingText + ' = ' + x + ' (unsigned) || ' + x1 + ' (signed)');
-      }
-    }
-    else if (/o[0-8_]+/g.test(leadingText)) {
-      const regex = /o([0-7_]+)/g;
-      let number = regex.exec(leadingText.replace('_', ''));
-      if (number === null || number[1] === null) {
-        return;
-      }
-      let x = parseInt(number[1], 8);
-      let x1 = eval_signed_oct(number[1], x);
-      if (x === x1) {
-        return new vscode.Hover(leadingText + ' = ' + x);
-      } else {
-        return new vscode.Hover(leadingText + ' = ' + x + ' (unsigned) || ' + x1 + ' (signed)');
-      }
-    }
-  }
-}
-
-
-function eval_signed_hex(number_s, int_number) {
-  let pow_hex = Math.pow(16, number_s.length);
-  let x1 = int_number;
-  if (int_number >= pow_hex >> 1) {
-    x1 = int_number - pow_hex;
-  }
-  return x1;
-}
-
-function eval_signed_bin(number_s, int_number) {
-  let pow_bin = 1 << number_s.length - 1;
-  let x1 = int_number;
-  if (int_number >= pow_bin >> 1) {
-    x1 = int_number - pow_bin;
-  }
-  return x1;
-}
-
-function eval_signed_oct(number_s, int_number) {
-  let pow_oct = Math.pow(8, number_s.length);
-  let x1 = int_number;
-  if (int_number >= pow_oct >> 1) {
-    x1 = int_number - pow_oct;
-  }
-  return x1;
 }
