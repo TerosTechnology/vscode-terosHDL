6d006ac893e74244bf06aa987cabcd5fe8737e9a
qarlosalberto <carlosruiznaranjo@gmail.com>
2023-08-06 13:37:40 +0200

    feat: use bootstrap in state machine viewer

--- packages/teroshdl/src/features/state_machine.ts
+++ packages/teroshdl/src/features/state_machine.ts
@@ -52,6 +52,8 @@ export class State_machine_manager extends Base_webview {
         const template_path = path_lib.join(this.context.extensionPath, 'resources', 'webviews', 'state_machine_viewer', 'index.html.nj');
         const template_str = fs.readFileSync(template_path, 'utf-8');
 
+        const css_bootstrap_path = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, 'resources', 'webviews', 'common',
+            'bootstrap.min.css'));
         const css_path = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, 'resources', 'webviews', 'state_machine_viewer',
             'style.css'));
         const js_path_0 = webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, 'resources', 'webviews',
@@ -66,7 +68,9 @@ export class State_machine_manager extends Base_webview {
             'state_machine_viewer', 'libs', 'viz.js'));
 
         const html = nunjucks.renderString(template_str, {
-            "css_path": css_path, "cspSource": webview.cspSource,
+            "css_path": css_path, 
+            "css_bootstrap_path": css_bootstrap_path, 
+            "cspSource": webview.cspSource,
             "js_path_0": js_path_0,
             "js_path_1": js_path_1,
             "js_path_2": js_path_2,
