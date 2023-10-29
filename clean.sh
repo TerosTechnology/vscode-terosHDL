46c9e936030de8e9299c87c3e78d550eb11d3ea1
asaezper <alfredo.saez@srs.io>
2023-10-29 19:42:50 +0100

    chore: scripts and vscode config

--- clean.sh
+++ clean.sh
@@ -1,8 +1,10 @@
-clean () {
-    rm -rf ./packages/$1/node_modules
-    rm -rf ./packages/$1/out
-    rm -rf ./packages/$1/yarn.lock
-    rm -rf ./packages/$1/package-lock.json
+#! /bin/bash
+
+clean() {
+    rm -rf "$(dirname "$0")/packages/$1/node_modules"
+    rm -rf "$(dirname "$0")/packages/$1/out"
+    rm -rf "$(dirname "$0")/packages/$1/yarn.lock"
+    rm -rf "$(dirname "$0")/packages/$1/package-lock.json"
 }
 
 clean "colibri"
