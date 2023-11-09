b2ae7429397282424c06eb25562154b6fed42a73
asaezper <alfredo.saez@srs.io>
2023-11-09 21:02:38 +0100

    chore(multi project manager): initialize project test

--- packages/colibri/tests/multi_project_manager/multi_project_manager.spec.ts
+++ packages/colibri/tests/multi_project_manager/multi_project_manager.spec.ts
@@ -16,113 +16,98 @@
 // You should have received a copy of the GNU General Public License
 // along with colibri2.  If not, see <https://www.gnu.org/licenses/>.
 
-import { assert } from 'chai';
-import { LANGUAGE } from '../../src/common/general';
-import { t_file } from '../../src/project_manager/common';
-import { Project_manager } from '../../src/project_manager/project_manager';
 import { Multi_project_manager } from '../../src/project_manager/multi_project_manager';
 
+describe('MultiProjectManager', () => {
+    let multiProjectManager: Multi_project_manager;
 
-const prj_0 = {
-    'name': 'prj-0',
-    'files': [
-        'file0', 'file1'
-    ]
-};
-
-const prj_1 = {
-    'name': 'prj-1',
-    'files': [
-        'file2'
-    ]
-};
-
-const prj_2 = {
-    'name': 'prj-2',
-    'files': [
-        'file3', 'file4', 'file5'
-    ]
-};
-
-// Create project manager
-const multi_prj = new Multi_project_manager("", "", undefined);
-
-function add_files(multi_manager: Multi_project_manager, prj: any) {
-    prj.files.forEach(function (file: any) {
-        const file_inst: t_file = {
-            name: file,
-            is_include_file: false,
-            include_path: "",
-            logical_name: "",
-            is_manual: true,
-            file_type: LANGUAGE.NONE,
-            file_version: undefined,
-        };
-        multi_manager.get_project_by_name(prj.name).add_file(file_inst);
-    });
-}
-
-function check_project(multi_manager: Multi_project_manager, expected_project: any) {
-    const prj = multi_manager.get_project_by_name(expected_project.name);
-    if (prj === undefined) {
-        assert.equal(true, false, "Prj doesn't exist.");
-    }
-    assert.equal(prj?.get_name(), expected_project.name, 'Fail in prj name.');
-    const prj_files = prj?.get_project_definition().file_manager.get();
-    assert.equal(prj_files?.length, expected_project.files.length, 'Fail in number of files');
-}
-
-describe(`Check multi project manager`, function () {
-
-    it(`Add 3 projects`, async function () {
-        // Create projects
-        multi_prj.initialize_project(prj_0.name);
-        multi_prj.initialize_project(prj_1.name);
-        multi_prj.initialize_project(prj_2.name);
-        // Add files
-        add_files(multi_prj, prj_0);
-        add_files(multi_prj, prj_1);
-        add_files(multi_prj, prj_2);
-        // Check
-        check_project(multi_prj, prj_0);
-        check_project(multi_prj, prj_1);
-        check_project(multi_prj, prj_2);
+    beforeEach(() => {
+        multiProjectManager = new Multi_project_manager("", "", undefined);
     });
 
-    it(`Rename project`, async function () {
-        const new_name = 'new-name';
-
-        // Rename project
-        const prj0_obj = multi_prj.get_project_by_name(prj_0.name);
-        multi_prj.rename_project(prj0_obj, new_name);
-
-        // Check
-        prj_0.name = new_name;
-        check_project(multi_prj, prj_0);
+    describe('default scenario', () => {
+        test('get_projects should return an empty array if no projects are created', () => {
+            expect(multiProjectManager.get_projects()).toEqual([]);
+        });
+        test('get_selected_project should throw an exception', () => {
+            expect(() => { multiProjectManager.get_selected_project() }).toThrow();
+        });
+        test('get_project_by_name should throw an exception when the project does not exit', () => {
+            expect(() => { multiProjectManager.get_project_by_name("") }).toThrow();
+        });
     });
 
-    it(`Delete one`, async function () {
-        // Delete project
-        const prj0_obj = multi_prj.get_project_by_name(prj_0.name);
-        multi_prj.delete_project(prj0_obj);
+    describe('initialize_project', () => {
 
-        // Check number of projects
-        assert.equal(multi_prj.get_projects().length, 2);
+        for (let num_projects = 1; num_projects <= 10; num_projects++) {
+            test(`should initialize ${num_projects} project(s) with different name(s)`, () => {
+                // Create each project, from 1 to num_projects
+                for (let i = 1; i <= num_projects; i++) {
+                    const project_name = `Project${i}`;
+                    multiProjectManager.initialize_project(project_name);
+                    // Assert created, last in the project_list and project_list size is ok
+                    expect(multiProjectManager.get_project_by_name(project_name)).toBeDefined();
+                    expect(multiProjectManager.get_projects()[i - 1].get_name()).toBe(project_name);
+                    expect(multiProjectManager.get_projects().length).toBe(i);
+                    // TODO check default values in Project object
+                }
+            });
+        }
 
-        // Check projects
-        check_project(multi_prj, prj_1);
-        check_project(multi_prj, prj_2);
+        test('should throw an error when initializing a project with a name that already exists', () => {
+            const projectName = 'DuplicateProject';
+            multiProjectManager.initialize_project(projectName);
+            expect(() => {
+                multiProjectManager.initialize_project(projectName);
+            }).toThrow();
+        });
+
+        test('should throw an error when initializing a project with an empty name', () => {
+            expect(() => {
+                multiProjectManager.initialize_project('');
+            }).toThrow();
+        });
+
+        test('should throw an error when initializing a project with a name that is only whitespace', () => {
+            expect(() => {
+                multiProjectManager.initialize_project('   ');
+            }).toThrow();
+        });
+
+        test('should throw an error when initializing a project with a name containing only special characters', () => {
+            expect(() => {
+                multiProjectManager.initialize_project('!@#$%^&*()');
+            }).toThrow();
+        });
+
+        test('should throw an error when initializing a project with a name longer than the maximum allowed length', () => {
+            const longName = 'a'.repeat(129); // Suponiendo que el máximo permitido es 255 caracteres
+            expect(() => {
+                multiProjectManager.initialize_project(longName);
+            }).toThrow();
+        });
+
+        test('should successfully initialize a project with a name of maximum allowed length', () => {
+            const maxName = 'a'.repeat(128); // Suponiendo que el máximo permitido es 255 caracteres
+            multiProjectManager.initialize_project(maxName);
+            const projects = multiProjectManager.get_projects();
+            expect(projects[projects.length - 1].get_name()).toBe(maxName);
+        });
+
+        test('should throw an error when initializing a project with a name that is a undefined', () => {
+            expect(() => {
+                multiProjectManager.initialize_project(undefined as any); // Casting a number to any to simulate wrong type input
+            }).toThrow();
+        });
+
+        test('should throw an error when initializing a project with a name that is an object', () => {
+            expect(() => {
+                multiProjectManager.initialize_project({ name: 'ObjectProject' } as any); // Casting an object to any to simulate wrong type input
+            }).toThrow();
+        });
+
+
     });
 
-    it(`Select project`, async function () {
-        // Select project
-        const prj1_obj = multi_prj.get_project_by_name(prj_1.name);
-        multi_prj.set_selected_project(prj1_obj);
-        assert.equal(multi_prj.get_selected_project(), prj1_obj);
 
-        // Select project doesn't exist
-        assert.Throw(() =>
-            multi_prj.set_selected_project(new Project_manager(prj_0.name, undefined))
-            , "Project new-name is not in the project list.");
-    });
 });
