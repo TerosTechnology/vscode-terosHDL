8afd7787220b8467693e17994232a8848b52c817
qarlosalberto <carlosruiznaranjo@gmail.com>
2023-12-24 11:30:29 +0100

    add decorator to timing paths

--- packages/teroshdl/src/views/timing/timing_report.ts
+++ packages/teroshdl/src/views/timing/timing_report.ts
@@ -20,6 +20,7 @@ import * as path_lib from 'path';
 import * as teroshdl2 from 'teroshdl2';
 import { t_Multi_project_manager } from '../../type_declaration';
 import { PathDetailsView } from './path_details';
+import { get_icon } from '../../features/tree_views/utils';
 
 export function getTimingReportView(context: vscode.ExtensionContext, projectManager: t_Multi_project_manager,
     pathDetailsView: PathDetailsView): TimingReportView {
@@ -47,6 +48,97 @@ export class TimingReportView implements vscode.WebviewViewProvider {
         this.pathDetailsView = pathDetailsView;
         this.context = context;
         this.projectManager = projectManager;
+
+
+        context.subscriptions.push(
+            vscode.window.onDidChangeActiveTextEditor((e) => this.createDecoratorList(e)),
+        );
+    }
+
+    private setDecoration(line: number, slack: number, setBackground: boolean, onlySlack: boolean) {
+        const activeEditor = vscode.window.activeTextEditor;
+        if (!activeEditor) {
+            return;
+        }
+
+        const message = new vscode.MarkdownString(
+            `Incremental delay: **${slack}**`,
+            true,
+        );
+
+        const range = new vscode.Range(line, 0, line, activeEditor.document.lineAt(line).text.length);
+        const decorationOptions = {
+            range: range,
+            hoverMessage: message
+        };
+
+        let icon = get_icon("clock").dark;
+        const currentTheme = vscode.window.activeColorTheme;
+        if (currentTheme.kind === vscode.ColorThemeKind.Dark) {
+            icon = get_icon("clock").dark;
+        } else if (currentTheme.kind === vscode.ColorThemeKind.Light) {
+            icon = get_icon("clock").light;
+        } else {
+            icon = get_icon("clock").dark;
+        }
+
+        let decoratorOptions = {
+            backgroundColor: '',
+            isWholeLine: true,
+            gutterIconPath: icon,
+            gutterIconSize: 'contain',
+            after: {}
+        };
+
+        if (setBackground) {
+            decoratorOptions.backgroundColor = 'rgba(255, 0, 0, 0.3)';
+        }
+
+        if (onlySlack) {
+            decoratorOptions.after = {
+                contentText: `Slack: ${slack}`,
+                margin: '0 0 0 3em',
+                color: 'rgba(255, 0, 0, 0.8)',
+                fontWeight: 'bold',
+            }
+        };
+        const decorationType = vscode.window.createTextEditorDecorationType(decoratorOptions);
+        activeEditor.setDecorations(decorationType, [decorationOptions]);
+    }
+
+    private createDecoratorList(document: vscode.TextEditor | undefined) {
+        try {
+            if (document === undefined) {
+                document = vscode.window.activeTextEditor;
+            }
+            if (document === undefined) {
+                return;
+            }
+            const filePath = document.document.fileName;
+
+            const lineRegistry: number[] = [];
+            for (const timingPath of this.timmingReport) {
+                const lineRegistryInPath: number[] = [];
+                // Fail path
+                if (timingPath.slack < 0) {
+                    for (const timingNode of timingPath.nodeList) {
+                        if (timingNode.path === filePath) {
+                            // if (!lineRegistryInPath.includes(timingNode.line)) {
+                            //     this.setDecoration(timingNode.line - 1, timingPath.slack,
+                            //         !lineRegistry.includes(timingNode.line), true);
+                            //     lineRegistryInPath.push(timingNode.line);
+                            // }
+
+                            this.setDecoration(timingNode.line - 1, timingNode.incremental_delay,
+                                !lineRegistry.includes(timingNode.line), false);
+                            lineRegistry.push(timingNode.line);
+                        }
+                    }
+                }
+            }
+        }
+        catch (error) {
+        }
     }
 
     resolveWebviewView(webviewView: vscode.WebviewView, context, token) {
@@ -123,6 +215,7 @@ export class TimingReportView implements vscode.WebviewViewProvider {
         }, async (progress) => {
             this.timmingReport = await selectProject.getTimingReport();
             this.sendTimingReport();
+            this.createDecoratorList(undefined);
         });
     }
 
