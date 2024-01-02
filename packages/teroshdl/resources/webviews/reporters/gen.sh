9ccf589a45ef3a2a608db19ee251ac936a6b0cb7
qarlosalberto <carlosruiznaranjo@gmail.com>
2024-01-02 18:29:49 +0100

    chore: refactor esbuild logs and timing

--- packages/teroshdl/resources/webviews/reporters/gen.sh
+++ packages/teroshdl/resources/webviews/reporters/gen.sh
@@ -1,12 +1,1 @@
-pwd=${PWD}
-
-build () {
-    cd ${pwd}
-    cd ${1}
-    echo "Building ${1}"
-    NODE_ENV=production node ./esbuild.js
-    cd ${pwd}
-}
-
-build "timing"
-build "logs"
+NODE_ENV=production node ./esbuild.js
