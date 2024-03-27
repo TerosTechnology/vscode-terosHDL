af25bf68e35a37da68131021d2f3ac18ee399edb
jdwatson513 <77299083+jdwatson513@users.noreply.github.com>
2024-03-27 10:19:03 -0500

    config settings updates  - support for tasks

--- packages/colibri/src/config/config_web.ts
+++ packages/colibri/src/config/config_web.ts
@@ -616,17 +616,22 @@ export const WEB_CONFIG = `
               </select>
             </div>
           
+          
+          
+          
             <div class="mb-3">
-            <label for="documentation-general-tasks" class="form-label">
-              Include tasks:
-              <span class="markConfig badge bg-secondary" id="mark_documentation-general-tasks"></span>
-            </label>
-            <select class="form-select" aria-label="Include tasks:" id="documentation-general-tasks">
-                    <option value='all'>All</option>
-                    <option value='only_commented'>Only commented</option>
-                    <option value='none'>None</option>
-            </select>
-          </div>
+              <label for="documentation-general-tasks" class="form-label">
+                Include tasks:
+                <span class="markConfig badge bg-secondary" id="mark_documentation-general-tasks"></span>
+              </label>
+              <select class="form-select" aria-label="Include tasks:" id="documentation-general-tasks">
+                      <option value='all'>All</option>
+                      <option value='only_commented'>Only commented</option>
+                      <option value='none'>None</option>
+              </select>
+            </div>
+          
+          
           
           
             <div class="mb-3">
@@ -4703,6 +4708,8 @@ export const WEB_CONFIG = `
     config["documentation"]["general"]["process"] = element_value
     element_value = document.getElementById("documentation-general-functions").value;
     config["documentation"]["general"]["functions"] = element_value
+    element_value = document.getElementById("documentation-general-tasks").value;
+    config["documentation"]["general"]["tasks"] = element_value
     element_value = document.getElementById("documentation-general-magic_config_path").value;
     config["documentation"]["general"]["magic_config_path"] = element_value
     config["editor"] = {}
@@ -5307,6 +5314,7 @@ export const WEB_CONFIG = `
     document.getElementById("documentation-general-types").value = config["documentation"]["general"]["types"];
     document.getElementById("documentation-general-process").value = config["documentation"]["general"]["process"];
     document.getElementById("documentation-general-functions").value = config["documentation"]["general"]["functions"];
+    document.getElementById("documentation-general-tasks").value = config["documentation"]["general"]["tasks"];
     document.getElementById("documentation-general-magic_config_path").value = config["documentation"]["general"]["magic_config_path"];
     document.getElementById("editor-general-stutter_comment_shortcuts").checked = config["editor"]["general"]["stutter_comment_shortcuts"];
     document.getElementById("editor-general-stutter_block_width").value = config["editor"]["general"]["stutter_block_width"];
@@ -5615,6 +5623,11 @@ export const WEB_CONFIG = `
       mark = MODIFIEDMSG;
     }
     document.getElementById("mark_documentation-general-functions").innerHTML = mark;
+    mark = "";
+    if (projectName !== undefined && config["documentation"]["general"]["tasks"] != undefined) {
+      mark = MODIFIEDMSG;
+    }
+    document.getElementById("mark_documentation-general-tasks").innerHTML = mark;
     mark = "";
     if (projectName !== undefined && config["documentation"]["general"]["magic_config_path"] != undefined) {
       mark = MODIFIEDMSG;
