7648592fcd114bafc59e854e604d8ec27120e3d8
Raj Aryan Singh <rajlysm15@gmail.com>
2024-07-10 17:27:19 +0530

    implement sandpiper logs to display output

---
+++ packages/teroshdl/src/features/tree_views/tasks/sandpiper_logger.ts
@@ -0,0 +1,23 @@
+import * as vscode from 'vscode';
+import { ProjectEmitter } from 'teroshdl2/out/project_manager/projectEmitter';
+import { e_event } from 'teroshdl2/out/project_manager/projectEmitter';
+
+const outputChannel = vscode.window.createOutputChannel('Sandpiper Logs');
+
+export function sandpiperLogger(emitterProject: ProjectEmitter) {
+    emitterProject.addProjectListener(async (log: string, type: e_event) => {
+        switch (type) {
+            case e_event.STDOUT_INFO:
+                outputChannel.appendLine(`INFO: ${log}`);
+                break;
+            case e_event.STDOUT_WARNING:
+                outputChannel.appendLine(`WARNING: ${log}`);
+                break;
+            case e_event.STDOUT_ERROR:
+                outputChannel.appendLine(`ERROR: ${log}`);
+                break;
+            default:
+                outputChannel.appendLine(log);
+        }
+    });
+}
