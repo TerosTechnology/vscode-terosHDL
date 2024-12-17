51ecf30fdf3c2f140f4264783443ac913d61dda1
AzarAI <azarai@mail.ustc.edu.cn>
2024-12-18 02:29:04 +0800

    allow custom version arguments for external tools
    - Added a `customVersionArgs` map to define tool-specific version arguments.
    - Default version argument is `--version` unless overridden by the map.
    - Enhanced flexibility to support tools like "vivado" requiring `-version`.
    - Simplified logic for future extensibility by allowing additional tool-argument pairs.

--- src/teroshdl/features/configChecker/externalTools.ts
+++ src/teroshdl/features/configChecker/externalTools.ts
@@ -37,9 +37,17 @@ export async function checkExternalToolManager(currentConfig: e_config) {
 
     msg += buildTitle('Checking External Tool Configuration', HELP);
 
+    // Build a custom map for specified extern tools' version check argument
+    const customVersionArgs: Record<string, string> = {
+        vivado: '-version',
+        // Add more tools and their custom arguments here
+        // "tool-name-example": "--example-arg"
+    };
+
     // Check external tool
     msg += `${INTROICON} Selected external tool: ${selectedTool.toLocaleUpperCase()}. Installation path: "${installationPath}"\n`;
-    let result = await checkBinary(selectedTool, installationPath, selectedTool.toLocaleLowerCase(), ['--version']);
+    const versionArgument = customVersionArgs[toolKey] || '--version'; // Default to '--version' if no custom argument is specified
+    let result = await checkBinary(selectedTool, installationPath, selectedTool.toLocaleLowerCase(), [versionArgument]);
     msg = appendMsg(result, msg, selectedTool.toLocaleUpperCase());
     msg += '\n';
     if (!result.successfulConfig) {
